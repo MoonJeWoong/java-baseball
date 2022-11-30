7334a5c9067ab2762d5ba44bb314729ddbbe887f
MoonJeWoong <munjin0201@naver.com>
2022-11-30 22:48:09 +0900

    feat(validators): 리스트가 자연수로 이루어진 리스트인지 검증한다.

--- src/main/java/baseball/validators/Validators.java
+++ src/main/java/baseball/validators/Validators.java
@@ -4,6 +4,8 @@ import java.util.HashSet;
 import java.util.List;
 
 public final class Validators {
+    private static final List<Integer> NATURAL_NUMBERS = List.of(1,2,3,4,5,6,7,8,9);
+
     public static void validateListSize(List<Integer> list, int size) throws IllegalArgumentException{
         if(list.size() != size){
             throw new IllegalArgumentException();
@@ -14,6 +16,14 @@ public final class Validators {
         HashSet<Integer> validationNumbers = new HashSet<>(list);
         if(validationNumbers.size() != list.size()){
             throw new IllegalArgumentException();
+        }
+    }
+
+    public static void validateNaturalNumberList(List<Integer> list) throws IllegalArgumentException{
+        for(int x : list){
+            if(!NATURAL_NUMBERS.contains(x)){
+                throw new IllegalArgumentException();
+            }
         }
     }
 }
