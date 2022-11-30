7ea0f80656e2a52c9aa4325ee5594f9d5fafc404
MoonJeWoong <munjin0201@naver.com>
2022-12-01 03:49:57 +0900

    feat(controller): 게임이 시작되기 전 컴퓨터의 수를 초기화한다.

--- src/main/java/baseball/controller/Controller.java
+++ src/main/java/baseball/controller/Controller.java
@@ -1,4 +1,18 @@
 package baseball.controller;
 
+import baseball.service.Service;
+import baseball.utils.NumberListGenerator;
+import baseball.utils.RandomNumberListGenerator;
+
 public class Controller {
+    private static final Service service = new Service();
+    private static final NumberListGenerator numberListGenerator = new RandomNumberListGenerator();
+
+    public void run(){
+        initGame();
+    }
+
+    private void initGame(){
+        service.setComputerNumbers(numberListGenerator);
+    }
 }
