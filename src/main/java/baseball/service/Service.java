7c73feee1d6b3d0055dfcee3199c22acfc7c6b71
MoonJeWoong <munjin0201@naver.com>
2022-12-01 00:59:14 +0900

    feat(service): 임의의 세자리 수로 컴퓨터의 수를 생성한다.

--- src/main/java/baseball/service/Service.java
+++ src/main/java/baseball/service/Service.java
@@ -1,4 +1,15 @@
 package baseball.service;
 
+import baseball.utils.NumberListGenerator;
+import baseball.domain.BaseballNumbers;
+
 public class Service {
+    private static final int NUMBERS_SIZE = 3;
+
+    private BaseballNumbers computerNumbers;
+
+    public void setComputerNumbers(NumberListGenerator numberListGenerator) throws IllegalArgumentException{
+        computerNumbers = BaseballNumbers.of(numberListGenerator.generate(NUMBERS_SIZE));
+    }
+
 }
