c9708533fae1ad855bd40aa6e8bdb8be37a1cff4
MoonJeWoong <munjin0201@naver.com>
2022-12-01 00:34:46 +0900

    feat(domain): 야구 게임에 사용되는 세자리 숫자를 저장한다.

--- src/main/java/baseball/domain/BaseballNumbers.java
+++ src/main/java/baseball/domain/BaseballNumbers.java
@@ -1,4 +1,26 @@
 package baseball.domain;
 
+import baseball.validators.Validators;
+
+import java.util.List;
+
 public class BaseballNumbers {
+    private static final int NUMBERS_SIZE = 3;
+
+    private final List<Integer> numbers;
+
+    public static BaseballNumbers of(List<Integer> numbers){
+        return new BaseballNumbers(numbers);
+    }
+
+    private BaseballNumbers(List<Integer> numbers) throws IllegalArgumentException{
+        validateNumbers(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validateNumbers(List<Integer> numbers) throws IllegalArgumentException{
+        Validators.validateListSize(numbers, NUMBERS_SIZE);
+        Validators.validateNoDuplicatedList(numbers);
+        Validators.validateNaturalNumberList(numbers);
+    }
 }
