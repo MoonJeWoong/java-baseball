dd35b7ab508f9ed4d2692f5591b35c53269a9c92
MoonJeWoong <munjin0201@naver.com>
2022-11-30 21:59:28 +0900

    feat(utils): 지정된 사이즈만큼의 무작위 숫자 리스트를 생성한다.

---
+++ src/main/java/baseball/utils/RandomNumberListGenerator.java
@@ -0,0 +1,25 @@
+package baseball.utils;
+
+import baseball.utils.NumberListGenerator;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RandomNumberListGenerator implements NumberListGenerator {
+
+    private static final int START_RANGE = 1;
+    private static final int END_RANGE = 9;
+
+    @Override
+    public List<Integer> generate(int size){
+        List<Integer> numbers = new ArrayList<>();
+        while (numbers.size() < size) {
+            int randomNumber = Randoms.pickNumberInRange(START_RANGE, END_RANGE);
+            if (!numbers.contains(randomNumber)) {
+                numbers.add(randomNumber);
+            }
+        }
+        return numbers;
+    }
+}
