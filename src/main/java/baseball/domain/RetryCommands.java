128a73f53f23631c22aef80359e2bf882cb225eb
MoonJeWoong <munjin0201@naver.com>
2022-12-01 14:56:17 +0900

    feat(domain): 게임 재시작 여부 입력값을 저장하고 반환한다.
    세부사항
    - 입력값에 따라 재시작 / 종료 명령어를 반환한다.
    - 입력값으로 1,2 이외 값이 주어지면 예외를 발생시킨다.

--- src/main/java/baseball/domain/RetryCommands.java
+++ src/main/java/baseball/domain/RetryCommands.java
@@ -1,4 +1,28 @@
 package baseball.domain;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public enum RetryCommands {
+
+    RETRY(1),
+    QUIT(2);
+
+    private int command;
+
+    RetryCommands(int command){
+        this.command = command;
+    }
+
+    public static RetryCommands from(int input) throws IllegalArgumentException{
+        return Arrays.stream(RetryCommands.values())
+                .filter(retryCommands -> Objects.equals(retryCommands.value(), input))
+                .findFirst()
+                .orElseThrow(()-> new IllegalArgumentException());
+    }
+
+    public int value(){
+        return this.command;
+    }
+
 }
