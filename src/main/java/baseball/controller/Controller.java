60363437cd78e6a4b269803daf91918aa0c6d010
MoonJeWoong <munjin0201@naver.com>
2022-12-01 16:48:50 +0900

    refactor(controller): 에러메시지 출력 방식 변경
    변경사항
    - 발생한 error 객체의 메시지를 받아서 outputView를 통해 메시지 출력

--- src/main/java/baseball/controller/Controller.java
+++ src/main/java/baseball/controller/Controller.java
@@ -9,7 +9,6 @@ import baseball.utils.Parser;
 import baseball.utils.RandomNumberListGenerator;
 import baseball.view.InputView;
 import baseball.view.OutputView;
-
 import java.util.List;
 
 public class Controller {
@@ -18,58 +17,58 @@ public class Controller {
     private static final InputView inputView = new InputView();
     private static final OutputView outputView = new OutputView();
 
-    public Controller(){
+    public Controller() {
         outputView.printGameStart();
     }
 
-    public void run(){
+    public void run() {
         initGame();
         playGame();
         retryGameOrQuit();
     }
 
-    private void initGame(){
+    private void initGame() {
         service.setComputerNumbers(numberListGenerator);
     }
 
-    private void playGame(){
+    private void playGame() {
         BaseballResults result = service.getBaseballResults(getPlayerNumbers());
         outputView.printGameResult(result);
         isGameEnded(result);
     }
 
-    private BaseballNumbers getPlayerNumbers(){
+    private BaseballNumbers getPlayerNumbers() {
         List<Integer> numbers = Parser.stringToIntegerList(inputView.readPlayerNumbers());
-        try{
+        try {
             return BaseballNumbers.of(numbers);
-        } catch (IllegalArgumentException error){
-            System.out.println("[ERROR] 숫자의 길이, 중복 여부, 자연수 여부를 확인 후 재입력 해 주십시오.");   //outputView 로 분리하기
+        } catch (IllegalArgumentException error) {
+            outputView.printErrorMessage(error.getMessage());
             return getPlayerNumbers();
         }
     }
 
-    private void isGameEnded(BaseballResults result){
-        if(!result.equals(BaseballResults.THREE_STRIKES)){
+    private void isGameEnded(BaseballResults result) {
+        if (!result.equals(BaseballResults.THREE_STRIKES)) {
             playGame();
             return;
         }
         outputView.printGameFinished();
     }
 
-    private void retryGameOrQuit(){
+    private void retryGameOrQuit() {
         RetryCommands retryCommands = getRetryCommands();
-        if(retryCommands.equals(RetryCommands.RETRY)){
+        if (retryCommands.equals(RetryCommands.RETRY)) {
             run();
             return;
         }
         outputView.printProgramQuit();
     }
 
-    private RetryCommands getRetryCommands(){
-        try{
+    private RetryCommands getRetryCommands() {
+        try {
             return RetryCommands.from(inputView.readRetryCommands());
         } catch (IllegalArgumentException error) {
-            System.out.println("[ERROR] 1 혹은 2의 숫자만 입력이 가능합니다. 재입력 해 주십시오.");   //outputView 로 분리하기
+            outputView.printErrorMessage(error.getMessage());
             return getRetryCommands();
         }
     }
