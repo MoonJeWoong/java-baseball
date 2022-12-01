23d3ebe212ea90ec74b5dd4d3262283f22225e1c
MoonJeWoong <munjin0201@naver.com>
2022-12-01 11:53:02 +0900

    test(domain): 스트라이크, 볼 개수에 따라 출력문구를 반환한다.

--- src/test/java/baseball/domain/BaseballResultsTest.java
+++ src/test/java/baseball/domain/BaseballResultsTest.java
@@ -19,6 +19,16 @@ class BaseballResultsTest {
         assertThat(result).isEqualTo(expected);
     }
 
+    @ParameterizedTest
+    @MethodSource("baseballResultsStringByStrikesAndBalls")
+    void 스트라이크_볼_개수에_따라_출력문구를_반환한다(int strikes, int balls, String expected){
+        //when
+        BaseballResults result = BaseballResults.getBaseballResults(strikes, balls);
+
+        //then
+        assertThat(result.toString()).isEqualTo(expected);
+    }
+
     static Stream<Arguments> baseballResultsByStrikesAndBalls(){
         return Stream.of(
                 Arguments.of(3,0,BaseballResults.THREE_STRIKES),
@@ -30,6 +40,20 @@ class BaseballResultsTest {
                 Arguments.of(0,2,BaseballResults.TWO_BALLS),
                 Arguments.of(0,3,BaseballResults.THREE_BALLS),
                 Arguments.of(0,0,BaseballResults.NOTHING)
+        );
+    }
+
+    static Stream<Arguments> baseballResultsStringByStrikesAndBalls(){
+        return Stream.of(
+                Arguments.of(3,0,"3스트라이크"),
+                Arguments.of(2,0,"2스트라이크"),
+                Arguments.of(1,0,"1스트라이크"),
+                Arguments.of(1,1,"1볼 1스트라이크"),
+                Arguments.of(1,2,"2볼 1스트라이크"),
+                Arguments.of(0,1,"1볼"),
+                Arguments.of(0,2,"2볼"),
+                Arguments.of(0,3,"3볼"),
+                Arguments.of(0,0,"낫싱")
         );
     }
 
