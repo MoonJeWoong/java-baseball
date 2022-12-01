0cc346cd7b4881bd23326e64d15ef6022b43b2ec
MoonJeWoong <munjin0201@naver.com>
2022-12-01 10:55:29 +0900

    feat(controller): 게임이 시작되면 플레이어의 숫자를 입력받는다.
    세부사항
    - 세자리 숫자 이외의 입력값이 들어오면 재입력받는다.

--- src/main/java/baseball/controller/Controller.java
+++ src/main/java/baseball/controller/Controller.java
@@ -1,18 +1,41 @@
 package baseball.controller;
 
+import baseball.domain.BaseballNumbers;
 import baseball.service.Service;
 import baseball.utils.NumberListGenerator;
+import baseball.utils.Parser;
 import baseball.utils.RandomNumberListGenerator;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
+import java.util.List;
 
 public class Controller {
     private static final Service service = new Service();
     private static final NumberListGenerator numberListGenerator = new RandomNumberListGenerator();
+    private static final InputView inputView = new InputView();
+    private static final OutputView outputView = new OutputView();
 
     public void run(){
         initGame();
+        playGame();
     }
 
     private void initGame(){
         service.setComputerNumbers(numberListGenerator);
+    }
+
+    private void playGame(){
+        getPlayerNumbers();
+    }
+
+    private BaseballNumbers getPlayerNumbers(){
+        List<Integer> numbers = Parser.stringToIntegerList(inputView.readPlayerNumbers());
+        try{
+            return BaseballNumbers.of(numbers);
+        } catch (IllegalArgumentException error){
+            System.out.println("[ERROR] 숫자의 길이, 중복 여부, 자연수 여부를 확인 후 재입력 해 주십시오.");   //outputView 로 분리하기
+            return getPlayerNumbers();
+        }
     }
 }
