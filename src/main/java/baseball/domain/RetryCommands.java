35858a9568b7e244beb5d6b73a53775247c99364
MoonJeWoong <munjin0201@naver.com>
2022-12-01 16:47:40 +0900

    refactor(all): 에러 메시지 상수화

--- src/main/java/baseball/domain/RetryCommands.java
+++ src/main/java/baseball/domain/RetryCommands.java
@@ -8,20 +8,22 @@ public enum RetryCommands {
     RETRY(1),
     QUIT(2);
 
-    private int command;
+    private static final String COMMAND_ERROR_MESSAGE = "[ERROR] 1 혹은 2의 숫자만 입력이 가능합니다. 재입력 해 주십시오.";
 
-    RetryCommands(int command){
+    private final int command;
+
+    RetryCommands(int command) {
         this.command = command;
     }
 
-    public static RetryCommands from(int input) throws IllegalArgumentException{
+    public static RetryCommands from(int input) throws IllegalArgumentException {
         return Arrays.stream(RetryCommands.values())
                 .filter(retryCommands -> Objects.equals(retryCommands.value(), input))
                 .findFirst()
-                .orElseThrow(()-> new IllegalArgumentException());
+                .orElseThrow(() -> new IllegalArgumentException(COMMAND_ERROR_MESSAGE));
     }
 
-    public int value(){
+    public int value() {
         return this.command;
     }
 
