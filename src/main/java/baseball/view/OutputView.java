215d9cb036d91b056b914ca3fac791b3a0fa8bbf
MoonJeWoong <munjin0201@naver.com>
2022-12-01 12:05:58 +0900

    feat(view): 플레이어 숫자 입력값에 대한 결과를 출력한다.

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,7 +1,13 @@
 package baseball.view;
 
+import baseball.domain.BaseballResults;
+
 public class OutputView {
     public void printGameStart(){
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public void printGameResult(BaseballResults result){
+        System.out.println(result.toString());
     }
 }
