a359e9be70f08285868c66bb1f6156ccf2f52aaa
MoonJeWoong <munjin0201@naver.com>
2022-12-01 10:43:32 +0900

    feat(view): 플레이어 숫자를 입력받는다.

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,4 +1,21 @@
 package baseball.view;
 
+import baseball.domain.BaseballNumbers;
+
+import java.util.Arrays;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class InputView {
+    public String readPlayerNumbers(){
+        System.out.print("숫자를 입력해주세요 : ");
+        String input = readLine();
+        try{
+            Integer.parseInt(input);
+        } catch (NumberFormatException error) {
+            System.out.println("[ERROR] 숫자로 이루어진 입력값이 아닙니다. 숫자만 입력해 주십시오.");
+            return readPlayerNumbers();
+        }
+        return input;
+    }
 }
