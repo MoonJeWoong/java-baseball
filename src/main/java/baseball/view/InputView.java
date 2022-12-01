592cd3c201ee520657f1b09b0d81b3a5963d1921
MoonJeWoong <munjin0201@naver.com>
2022-12-01 14:38:54 +0900

    feat(view): 게임 재시작 / 종료 여부를 입력받는다.

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,6 +1,7 @@
 package baseball.view;
 
 import baseball.domain.BaseballNumbers;
+import baseball.domain.RetryCommands;
 
 import java.util.Arrays;
 
@@ -17,5 +18,15 @@ public class InputView {
             return readPlayerNumbers();
         }
         return input;
+    }
+
+    public int readRetryCommands(){
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        try{
+            return Integer.parseInt(readLine());
+        } catch (NumberFormatException error) {
+            System.out.println("[ERROR] 유효한 입력값이 아닙니다. 1과 2중 하나의 숫자만 입력해 주십시오.");
+            return readRetryCommands();
+        }
     }
 }
