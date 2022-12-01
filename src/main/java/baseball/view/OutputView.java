e08471d73216248fffe892cf777dacbc7b4a84a7
MoonJeWoong <munjin0201@naver.com>
2022-12-01 14:38:14 +0900

    feat(view): 프로그램 종료 문구를 출력한다.

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -14,4 +14,8 @@ public class OutputView {
     public void printGameFinished(){
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
+
+    public void printProgramQuit(){
+        System.out.println("프로그램이 종료되었습니다. 감사합니다.");
+    }
 }
